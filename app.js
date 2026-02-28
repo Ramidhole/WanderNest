9716b12f08ccb027b37166c0af1e463736378c02
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2026-02-28 20:50:41 +0530

    connect database

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const ejs= require("ejs")
-
 const app = express()
+const Mngo_url ="mongodb://127.0.0.1:27017/wandernest"
+
+
+async function main(){
+    await mongoose.connect(Mngo_url)
+}
+main()
+.then(()=>{
+    console.log("connected to DB")
+})
+.catch((err)=>{
+    console.log(err)
+})
 
 app.get("/",(req,res)=>{
     res.send("hi i am root ")
