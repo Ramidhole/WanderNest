0e641ddcb51369b1258be4eebd6ce968fb4f2c4a
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2026-03-04 20:27:58 +0530

    Initialize and add data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const ejs= require("ejs")
 const app = express()
 const Mngo_url ="mongodb://127.0.0.1:27017/wandernest"
+const Listing = require("./models/listing")
 
 
 async function main(){
@@ -18,6 +19,20 @@ main()
 
 app.get("/",(req,res)=>{
     res.send("hi i am root ")
+})
+
+app.get("/testListing", async(req,res)=>{
+    let sampleListing = new Listing({
+        title:"my new villa",
+        description:"by the beach",
+        price:2000,
+        location:"calangute goa",
+        country:"India"
+    });
+
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("sample was saved");
 })
 
 app.listen(1000,()=>{
