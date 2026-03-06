69a87df5c860f13448a7bbd4a30943d4b40342be
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2026-03-06 23:32:21 +0530

    add new Listing and create routes

--- app.js
+++ app.js
@@ -44,12 +44,21 @@ const allListing = await Listing.find({})
 res.render("listings/index.ejs",{allListing})
 })
 
+app.get("/listings/new", (req,res)=>{
+    res.render("listings/new.ejs")
+})
+
 app.get("/listings/:id", async(req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing})
 })
 
+app.post("/listings",async(req,res)=>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+})
 
 app.listen(1000,()=>{
     console.log("app is listen port 1000")
